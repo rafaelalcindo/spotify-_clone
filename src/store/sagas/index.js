afca64b1003a5b2dff16f5abd24fcf6dc0a67859
Rafael Alcindo <rafael_alcindo@hotmail.com>
2020-10-27 10:36:13 -0300

    ajustando Saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,12 @@
-import { all } from 'redux-saga/effects'
+import { all, takeLatest } from 'redux-saga/effects'
+import { Playlist } from '../../pages/browse/styles'
+
+import { Types as PlaylistsTypes } from '../ducks/playlists'
+
+import { getPlaylists } from './playlists'
 
 export default function* rootSaga() {
-  yield all([])
+  yield all([
+    takeLatest(PlaylistsTypes.GET_REQUEST, getPlaylists)
+  ])
 }
